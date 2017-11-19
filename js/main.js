613e5203a9d84e261b331fe0a074eaa210792b94
ferndragon <ferndragon64@yahoo.com>
2017-11-19 11:01:42 -0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+setTimeout(function() {
+	//document.body.style.backgroundImage = 'url(img/cubey_cake-skull.jpg)';
+	 document.querySelector('.image2').style.opacity = 1;
+},3000);
+
+setTimeout(function() {
+	 document.querySelector('.image3').style.opacity = 1;
+},8000);
+
+setTimeout(function() {
+	 document.querySelector('.image4').style.opacity = 1;
+},13000);
+
+setTimeout(function() {
+	 document.querySelector('.image5').style.opacity = 1;
+},18000);
